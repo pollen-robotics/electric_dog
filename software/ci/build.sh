f50c4b9d097de364142a8d8f6f41140f399e1453
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-31 16:03:20 +0100

    Remove clippy in the mandatory build.
    Will add it as an optional build later.

--- software/ci/build.sh
+++ software/ci/build.sh
@@ -2,9 +2,9 @@
 
 pushd software
   if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-    cargo build --features "clippy" --examples
+    cargo build --examples
 
   elif [ $TARGET = thumbv6m-none-eabi ]; then
-    xargo build --features "clippy" --target $TARGET --examples
+    xargo build --target $TARGET --examples
   fi
 popd
