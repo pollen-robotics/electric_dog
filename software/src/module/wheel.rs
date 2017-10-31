6a4306680dd74a8f244199a4b5c63969bbb42da8
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-31 17:39:21 +0100

    Add pin settings for the wheels.

--- software/src/module/wheel.rs
+++ software/src/module/wheel.rs
@@ -4,19 +4,27 @@ use hal::gpio;
 /// Wheel for Electric Dog
 pub struct Wheel {
     name: &'static str,
+    pin: gpio::Output,
 }
 
 impl Wheel {
     /// Setup a Wheel and attach a Servo to the `Pin`
-    pub fn new(name: &'static str, _pin: gpio::Pin) -> Wheel {
-        Wheel { name }
+    pub fn new(name: &'static str, pin: gpio::Pin) -> Wheel {
+        Wheel {
+            name,
+            pin: gpio::Output::setup(pin),
+         }
     }
 
     /// Make the wheel turn (using the pre-defined speed)
-    pub fn on(&self) {}
+    pub fn on(&mut self) {
+        self.pin.high();
+    }
 
     /// Stop the wheel
-    pub fn off(&self) {}
+    pub fn off(&mut self) {
+        self.pin.low();
+    }
 }
 
 impl luos::Module for Wheel {
